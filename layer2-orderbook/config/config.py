37d946f75011834d9e47fbfd4892622fc657aaab
a18nikhil <a17nikhil@gmail.com>
2026-06-03 15:34:32 +0530

    Update config.py

---
+++ layer2-orderbook/config/config.py
@@ -0,0 +1,10 @@
+BASE_DIR = "."
+DATA_DIR = "./data"
+
+EXCHANGE = "binance_futures"
+SYMBOL = "btcusdt"
+
+WS_URL = "wss://fstream.binance.com/ws"
+REST_URL = "https://fapi.binance.com/fapi/v1/depth"
+
+DEPTH_LIMIT = 1000
